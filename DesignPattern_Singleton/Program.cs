9f53a25c835ca98f29f6c449f9f37e7cadc565f1
davidallenbaer <davidallenbaer@gmail.com>
2021-05-16 18:51:50 -0400

    Add project files.

---
+++ DesignPattern_Singleton/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern_Singleton
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+                Singleton fromTeachaer = Singleton.GetInstance;
+                fromTeachaer.PrintDetails("From Teacher");
+                Singleton fromStudent = Singleton.GetInstance;
+                fromStudent.PrintDetails("From Student");
+                Console.ReadLine();
+        }
+    }
+}
